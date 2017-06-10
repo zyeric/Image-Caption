418dbbb3b1593dd589828e3c07978ef7a1b7ae4f
zyeric <619828575@qq.com>
2017-06-10 20:39:43 +0800

    添加了符合网页测试输出格式的代码

--- Source Code/im2txt/Tensorflow/run_inference.py
+++ Source Code/im2txt/Tensorflow/run_inference.py
@@ -6,34 +6,47 @@ import tensorflow as tf
 import caption_generator
 import math
 
-vocab = vocabulary.Vocabulary("data/dictionary.txt")
+vocab = vocabulary.Vocabulary("data/segment/dictionary.txt")
+file = h5py.File("data/image_vgg19_fc1_feature.h5", 'r')
+encoded_images = file['validation_set']
 
-print(vocab.start_id)
-
-print(vocab.end_id)
-
-print(vocab.unk_id)
+check_point_steps = 393000
 
 model = inference_wrapper.InferenceWrapper()
 restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
-                                               "train_log/393000.ckpt")
-
-file = h5py.File("data/image_vgg19_fc1_feature.h5", 'r')
-encoded_images = file['train_set']
+                                               "check_point_files/segment/393000_steps/{}.ckpt".format(check_point_steps))
 
 sess = tf.InteractiveSession()
 restore_fn(sess)
 
-
-
 generator = caption_generator.CaptionGenerator(model, vocab)
 
+# 下面的代码输出了一个beam=3的所有内容
+# with open('train_caption_output.txt', 'w') as f:
+#     for iter in range(400):
+#         index = 2 + iter * 20
+#         captions = generator.beam_search(sess, encoded_images[index])
+#         f.write("Captions for image {}\r\n".format(index + 1))
+#         print("Captions for image {}".format(index + 1))
+#         for i, caption in enumerate(captions):
+#             # Ignore begin and end words.
+#             sentence = [vocab.id_to_word(w - 1) for w in caption.sentence[1:-1]]
+#             sentence = " ".join(sentence)
+#             f.write("  %d) %s (p=%f)\r\n" % (i, sentence, math.exp(caption.logprob)))
+#             print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
 
-for index in range(30):
-    captions = generator.beam_search(sess, encoded_images[index])
-    print("Captions for image {}".format(index + 1))
-    for i, caption in enumerate(captions):
-        # Ignore begin and end words.
+# 下面的代码按网络评分格式输出到文件
+with open('D:\Image-Caption\Results\\validation_output\\valid_caption_output_segment_{}_emb_512.txt'.format(check_point_steps),
+            'w') as f:
+    for index in range(1000):
+        captions = generator.beam_search(sess, encoded_images[index])
+        caption = captions[0]
+        #print(caption.sentence)
         sentence = [vocab.id_to_word(w - 1) for w in caption.sentence[1:-1]]
-        sentence = " ".join(sentence)
-        print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
+        full_str = "".join(sentence)
+        f.write(repr(8000 + index))
+        for word in full_str:
+            f.write(' {}'.format(word))
+        f.write('\n')
+        if (index + 1) % 100 == 0:
+            print(repr(index + 1))
